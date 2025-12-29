8b625742b136257a07c3d6b8f9d978bf18684cca
Ubaid Siddiqui <ubaidsid99@gmail.com>
2025-12-29 22:16:44 +0530

    ag changes

--- app/src/main/java/com/learning/appusagestats/MainActivity.kt
+++ app/src/main/java/com/learning/appusagestats/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { // aid
+        setContent { // aid, from ag
             AppUsageStatsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
