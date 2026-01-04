62c1292634939236a38bb4d3d42165c4a2657216
Ubaid Siddiqui <ubaidsid99@gmail.com>
2026-01-04 12:43:26 +0530

    - added unit and ui test files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("app.cash.turbine:turbine:1.0.0")
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
